3cb42509a9d367bf77e1486b7fa29bcb98e33b69
hoa.le <hoa.le@newoceaninfosys.com>
2016-06-17 15:31:58 +0700

    pop up modal

--- app/product/product.component.ts
+++ app/product/product.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit, Input} from '@angular/core';
-
-
+import {MODAL_DIRECTVES, BS_VIEW_PROVIDERS} from 'ng2-bootstrap/ng2-bootstrap';
 @Component({
     selector: 'product',
     templateUrl: 'app/product/product.template.html',
+    directives: [MODAL_DIRECTVES],
+    viewProviders:[BS_VIEW_PROVIDERS],
 
 })
 export class ProductComponent  {
